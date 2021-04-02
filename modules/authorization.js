2cf5fbbb14f9162bf4beb24cee8c9448e756bf29
yuanqingyan <17852613880@163.com>
2021-04-02 21:33:00 +0800

    token

--- modules/authorization.js
+++ modules/authorization.js
@@ -1,13 +1,18 @@
 var path = require('path');
 
+var jwt = require('jsonwebtoken');
+var jwt_config = require('config').get('jwt_config');
 
-module.exports.getService = (serviceName) => {
-  // 校验通过
+function createToken(uid, sub) {
+  var token = jwt.sign({ "uid": uid, "sub": sub }, jwt_config.get("secretKey"), { "expiresIn": jwt_config.get("expiresIn") });
+  return "Bearer " + token;
+}
 
-  var serviceModule = path.join(process.cwd(),`services/${serviceName}`);
-  if (!serviceModule) {
-    console.error(`${serviceName}模块不存在`);
-    return null;
-  }
-  return serviceModule;
+/**
+ * 
+ * @param {*} uid 签发人
+ * @param {*} sub 主题 默认为local_token
+ */
+module.exports.getToken = (uid, sub = 'local_token') => {
+  return createToken(uid, sub);
 }
---
+++ test/test.js
@@ -0,0 +1,6 @@
+
+var authorization = require('../modules/authorization');
+
+var token = authorization.getToken('yqy');
+
+console.log(token);
