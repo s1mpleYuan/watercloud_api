c799771976b48f4f1c1673cbf80e1ebdf030fca1
yuanqingyan <17852613880@163.com>
2021-04-03 09:59:37 +0800

    token校验

--- modules/authorization.js
+++ modules/authorization.js
@@ -3,16 +3,9 @@ var path = require('path');
 var jwt = require('jsonwebtoken');
 var jwt_config = require('config').get('jwt_config');
 
-function createToken(uid, sub) {
+
+
+module.exports.createToken = (uid, sub) => {
   var token = jwt.sign({ "uid": uid, "sub": sub }, jwt_config.get("secretKey"), { "expiresIn": jwt_config.get("expiresIn") });
   return "Bearer " + token;
-}
-
-/**
- * 
- * @param {*} uid 签发人
- * @param {*} sub 主题 默认为local_token
- */
-module.exports.getToken = (uid, sub = 'local_token') => {
-  return createToken(uid, sub);
 }
